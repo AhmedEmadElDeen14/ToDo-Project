38d8314470700080b7f44323db7c61c2994eeb98
AhmedEmadElDeen14 <ahmedemadeldeen0@gmail.com>
2024-02-27 13:47:44 +0200

    Update Home Screen with App Bar and Bottom Navigation Bar

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 
-class HomeScreen extends StatelessWidget{
-  static const String routeName="HomeScreen";
+class HomeScreen extends StatefulWidget {
+  static const String routeName = "HomeScreen";
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "To Do List",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        backgroundColor: Color(0xff5D9CEC),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 10,
+        shape: CircularNotchedRectangle(),
+        padding: EdgeInsets.all(1),
+        child: BottomNavigationBar(
+          selectedItemColor: Color(0xff5D9CEC),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: index,
+          onTap: (value) {
+            index = value;
+            setState(() {});
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.list,
+                size: 35,
+              ),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings_outlined,
+                size: 35,
+              ),
+              label: "",
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 35,
+        ),
+        backgroundColor: Color(0xff5D9CEC),
+        shape: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(35),
+            borderSide: BorderSide(
+              color: Colors.white,
+              width: 3,
+            )),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
-
 }
