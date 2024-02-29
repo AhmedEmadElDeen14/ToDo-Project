7b3fdf29d92689deb6253b3b65c298459d7b9863
AhmedEmadElDeen14 <ahmedemadeldeen0@gmail.com>
2024-02-29 22:07:07 +0200

    Added Timer Method to splash screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,38 +1,56 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:todo_app/home.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   static const String routeName = "SplashScreen";
 
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () {
+        Navigator.of(context).pushReplacementNamed(HomeScreen.routeName);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Center(
+          const Center(
             child: Image(
               image: AssetImage("assets/images/logo.png"),
             ),
           ),
           Container(
             alignment: Alignment.bottomCenter,
-            child: Image(
+            child: const Image(
               image: AssetImage("assets/images/route blue.png"),
             ),
           ),
           Container(
             alignment: Alignment.bottomCenter,
-            padding: EdgeInsets.symmetric(vertical: 15),
+            padding: const EdgeInsets.symmetric(vertical: 15),
             child: Text(
               "Supervised by Mohamed Hamoda",
               style: GoogleFonts.poppins(
                 fontSize: 14,
                 fontWeight: FontWeight.w400,
-                color: Color(0xff5D9CEC),
+                color: const Color(0xff5D9CEC),
               ),
-
             ),
           ),
         ],
